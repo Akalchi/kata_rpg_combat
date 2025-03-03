3f031db328bf4031597b6d1146b54d75094033a4
Akalchi <alejandrafdez1699@gmail.com>
2025-03-03 12:32:44 +0100

    "FEAT [LEVEL1] implemented - attribute, constructor and getter"

--- src/main/java/dev/alejandra/models/Character.java
+++ src/main/java/dev/alejandra/models/Character.java
@@ -3,8 +3,18 @@ package dev.alejandra.models;
 public class Character {
     
     private int health = 1000;
+    private int level = 1;
+
+    public Character() {
+        this.health = 1000;
+        this.level = 1;
+    }
     
     public int getHealth() {
         return health;
+    }
+
+    public int getLevel() {
+        return level;
     }
 }
